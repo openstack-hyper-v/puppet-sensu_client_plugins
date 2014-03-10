a739ef3ca5debd3e1bcae17ce9b4f8fa7511e6a7
Tim Rogers <tiroger@microsoft.com>
2014-03-10 16:00:17 -0700

    fixing reource creation

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,7 @@
 #
 #
 #
-# $repos - Hash of plugin repos to clone.  Key is "friendly name", value is git repo url.
+# $repos - Hash of plugin repos to clone.
 #
 
 class sensu_client_plugins (
@@ -10,35 +10,51 @@ class sensu_client_plugins (
 
   case $::osfamily {
     'windows': {
-      $checkout_loc = 'C:/ProgramData/sensu_plugins'
-      $sensu_plugins_loc = 'c:/etc/sensu/plugins'
+      $locations = {
+        "checkout_loc" => 'C:/ProgramData/sensu_plugins',
+        "sensu_plugins_loc" => 'c:/etc/sensu/plugins',
+      }
     }
     default: {
-      $checkout_loc = '/usr/local/src/sensu_plugins'
-      $sensu_plugins_loc = '/etc/sensu/plugins'
+      $locations = {
+        "checkout_loc" => '/usr/local/src/sensu_plugins',
+        "sensu_plugins_loc" => '/etc/sensu/plugins',
+      }
     }
   }
 
-  file {$checkout_loc:
+  file {$locations["checkout_loc"]:
     ensure => directory,
   }
-  file {$sensu_plugins_loc:
+  file {$locations["sensu_plugins_loc"]:
     ensure => directory,
   }
 
-  vcsrepo {keys($repos):
+  create_resources("sensu_plugin_repo", $repos, $locations)
+}
+
+define sensu_plugin_repo (
+  $repo_name = $title,
+  $repo_source,
+  $checkout_loc,
+  $sensu_plugins_loc,
+){
+  vcsrepo {$repo_name:
     ensure      => 'latest',
     revision    => 'origin/HEAD',
-    path        => "${checkout_loc}/${title}",
-    source      => $repos[$title],
+    path        => "${checkout_loc}/${repo_name}",
+    source      => $repo_source,
     provider    => 'git',
+    require => File[$checkout_loc, $sensu_plugins_loc],
+#    before  => File[$sensu_plugins_loc],
   }
-  
-  file {keys($repos):
-    path    => "${sensu_plugins_loc}/${title}",
+
+  file {$repo_name:
+    path    => "${sensu_plugins_loc}/${repo_name}",
     ensure  => link,
-    target  => "${checkout_loc}/${title}",
-    require => [File[$checkout_loc, $sensu_plugins_loc],Vcsrepo[${title}]],
+    target  => "${checkout_loc}/${repo_name}",
+#    require => File[$sensu_plugins_loc],
+    require => Vcsrepo[$repo_name],
   }
 
 }
